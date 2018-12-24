974fb50543bc8bf52dc3c1bf64a850c92dd935f7
xena <kakl2612@gmail.com>
2018-12-25 04:44:37 +0900

    head

--- src/main/scala/param/LitValueParameter.scala
+++ src/main/scala/param/LitValueParameter.scala
@@ -1,3 +1,12 @@
+/*
+ *
+ *   Copyright 2018 Xena.
+ *
+ *   This software is released under the MIT License.
+ *   http://opensource.org/licenses/mit-license.php
+ *
+ */
+
 package param
 
 import org.apache.spark.sql.functions._
--- src/main/scala/result/Result.scala
+++ src/main/scala/result/Result.scala
@@ -1,3 +1,12 @@
+/*
+ *
+ *   Copyright 2018 Xena.
+ *
+ *   This software is released under the MIT License.
+ *   http://opensource.org/licenses/mit-license.php
+ *
+ */
+
 package result
 
 import org.apache.spark.sql.DataFrame
